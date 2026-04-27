262ed8feef0ecb7acfd1fb8349b24bfbb5459d74
Britto1-code <pauljoshy8@gmail.com>
2026-04-27 19:36:56 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 # Load model
-with open("churn_model.pkl", "rb") as f:
+with open("customer_churn_model.pkl", "rb") as f:
     model_data = pickle.load(f)
 
 weights = model_data["weights"]
